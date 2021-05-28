0d6a821897b80a0e0d4c3cfe6a0af5752ea28c21
Ian <ianelicha@gmail.com>
2021-05-27 21:48:53 -0600

    Added link copying to blogviewer

--- components/BlogViewer.tsx
+++ components/BlogViewer.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import { Avatar } from "./Avatar";
 import { Title } from "./Title";
 import styles from "../styles/Blog.module.css";
+import { darkCtx } from "./context/useDarkTheme";
 
 export interface BVProps {
+  id: string;
   title: string;
   image: string;
   author: string;
@@ -12,7 +14,27 @@ export interface BVProps {
   article: string;
 }
 
-export function BlogViewer({ image, title, avatar, article, date }: BVProps) {
+export function BlogViewer({
+  id,
+  image,
+  title,
+  avatar,
+  article,
+  date,
+  author,
+}: BVProps) {
+  const [clicked, setClicked] = useState(false);
+  function onCopy() {
+    navigator.clipboard.writeText(`${document.location.host}/blog/${id}`);
+    setClicked(true);
+  }
+  const [{ dark }] = useContext(darkCtx);
+
+  useEffect(() => {
+    const k = setTimeout(() => void setClicked(false), 1500);
+    return () => void clearTimeout(k);
+  }, [clicked]);
+
   return (
     <div>
       <section className="relative h-96">
@@ -33,6 +55,23 @@ export function BlogViewer({ image, title, avatar, article, date }: BVProps) {
         </header>
       </section>
 
+      <div className="flex items-center p-5 text-lg">
+        <img
+          src={clicked ? "/check.svg" : "/link.svg"}
+          onClick={onCopy}
+          style={{
+            filter: !dark
+              ? "invert(0%) sepia(0%) saturate(0%) hue-rotate(21deg) brightness(100%) contrast(103%)"
+              : "invert(100%) sepia(100%) saturate(0%) hue-rotate(96deg) brightness(105%) contrast(104%)",
+          }}
+          className={`p-1 m-1 cursor-pointer rounded-md ${
+            clicked ? "bg-green-500" : ""
+          } transition`}
+        />
+        <h1 className="font-bold mr-2">{author}</h1>-
+        <h1 className="ml-2 font-thin opacity-70">{date}</h1>
+      </div>
+
       <article
         dangerouslySetInnerHTML={{
           __html: article,
@@ -43,6 +82,7 @@ export function BlogViewer({ image, title, avatar, article, date }: BVProps) {
           styles.quote,
           styles.image,
           styles.code,
+          styles.text,
           "p-2",
         ].join(" ")}
       />
