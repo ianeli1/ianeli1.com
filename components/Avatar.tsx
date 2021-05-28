cf71696a5e6183173ab04af43a02e77172b0a053
Ian <ianelicha@gmail.com>
2021-05-27 20:09:14 -0600

    Rounded avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -7,7 +7,10 @@ interface AvatarProps {
 
 export function Avatar({ image, size = "16rem" }: AvatarProps) {
   return (
-    <div className="rounded-full" style={{ width: size, height: size }}>
+    <div
+      className="rounded-full overflow-hidden"
+      style={{ width: size, height: size }}
+    >
       <img src={image} className="w-full h-full object-cover" />
     </div>
   );
