a9287063dc0ca83a2804a93f21e8df608a3b1d20
Ian <ianelicha@gmail.com>
2021-06-14 00:37:59 -0600

    Use localstorage to save dark theme preference

--- components/context/useDarkTheme.tsx
+++ components/context/useDarkTheme.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 interface DarkContext {
   dark: boolean;
@@ -10,8 +10,16 @@ export const darkCtx = createContext<
 
 export function DarkThemeProvider({ children }: React.PropsWithChildren<{}>) {
   const [dark, setDark] = useState<DarkContext>({
-    dark: true,
+    dark: false,
   });
+
+  useEffect(() => {
+    setDark({ dark: localStorage.getItem("dark") === "true" });
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("dark", dark.dark ? "true" : "false");
+  }, [dark]);
 
   return (
     <darkCtx.Provider value={[dark, setDark]}>{children}</darkCtx.Provider>
