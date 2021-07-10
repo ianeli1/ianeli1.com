6c53a1d9fe967459ab072c29301350ba60bd8699
Ian <ianelicha@gmail.com>
2021-07-09 22:57:47 -0600

    Allow scroll to be disabled via context

--- components/layout/ScrollPages.tsx
+++ components/layout/ScrollPages.tsx
@@ -1,26 +1,47 @@
-import { useRef, useCallback, useEffect } from "react";
+import {
+  useRef,
+  useCallback,
+  useEffect,
+  createContext,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from "react";
 
 //implement touch events https://developer.mozilla.org/en-US/docs/Web/API/Touch_events
+
+export const EnableScrollCtx = createContext<
+  [boolean, Dispatch<SetStateAction<boolean>>]
+>(undefined!);
 
 export const ScrollPages: React.FC<{ children: React.ReactNode[] }> = ({
   children,
 }) => {
+  const [enable, setEnable] = useState(true);
+
   const mainRef = useRef<HTMLElement>();
   const pagesRef = useRef<HTMLDivElement[]>([]);
   const counter = useRef(0);
-  const scrollEffect = useCallback((e: WheelEvent) => {
-    //>0 down <0 up
-    counter.current += e.deltaY < 0 ? -1 : 1;
-    if (counter.current < 0) counter.current = 0;
-    if (counter.current >= children.length)
-      counter.current = children.length - 1;
-    pagesRef.current[counter.current].scrollIntoView({ behavior: "smooth" });
-  }, []);
+  const scrollEffect = useCallback(
+    (e: WheelEvent) => {
+      //>0 down <0 up
+      if (enable) {
+        counter.current += e.deltaY < 0 ? -1 : 1;
+        if (counter.current < 0) counter.current = 0;
+        if (counter.current >= children.length)
+          counter.current = children.length - 1;
+        pagesRef.current[counter.current].scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    },
+    [enable]
+  );
   useEffect(() => {
     const { current } = mainRef;
     current?.addEventListener("wheel", scrollEffect);
-    return () => current?.removeEventListener("scroll", scrollEffect);
-  }, [mainRef.current]);
+    return () => current?.removeEventListener("wheel", scrollEffect);
+  }, [mainRef.current, enable]);
 
   return (
     <main
@@ -28,15 +49,17 @@ export const ScrollPages: React.FC<{ children: React.ReactNode[] }> = ({
       style={{ height: "calc(100vh - 8rem)" }}
       ref={(r) => r && (mainRef.current = r)}
     >
-      {children.map((node, i) => (
-        <div
-          key={i}
-          className="h-full"
-          ref={(r) => r && (pagesRef.current[i] = r)}
-        >
-          {node}
-        </div>
-      ))}
+      <EnableScrollCtx.Provider value={[enable, setEnable]}>
+        {children.map((node, i) => (
+          <div
+            key={i}
+            className="h-full"
+            ref={(r) => r && (pagesRef.current[i] = r)}
+          >
+            {node}
+          </div>
+        ))}
+      </EnableScrollCtx.Provider>
     </main>
   );
 };
