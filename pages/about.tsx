b63e7d91e5faf2b5e1361bed30baddc7dab1a8ad
Ian <ianelicha@gmail.com>
2021-07-10 22:36:18 -0600

    Delete unneeded calc

--- pages/about.tsx
+++ pages/about.tsx
@@ -19,12 +19,7 @@ export default function About() {
       <Gallery
         right
         background={
-          <main
-            className="h-full w-1/3 flex justify-center items-center"
-            style={{ marginLeft: "66.666667%" }}
-          >
-            <Avatar image="https://cdn.discordapp.com/attachments/616319929532022796/847658206725668894/1612222001434.png" />
-          </main>
+          <Avatar image="https://cdn.discordapp.com/attachments/616319929532022796/847658206725668894/1612222001434.png" />
         }
       >
         <ScrollPages>
