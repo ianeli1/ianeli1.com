6eec00a8e799932b31bf9e59aa8e41bf379c6dab
FF_Ian <ian@firstfactory.com>
2021-11-05 18:32:23 -0600

    Add an optional margin parameter

--- components/FloatingCard/index.tsx
+++ components/FloatingCard/index.tsx
@@ -4,20 +4,25 @@ import { Title } from "../Title";
 import classes from "./styles.module.scss";
 
 export interface IFloatingCardProps {
-  image: string;
+  image?: string;
   title: string;
   children: React.ReactNode;
+  margin?: string;
 }
 
 export const FloatingCard: FC<IFloatingCardProps> = ({
   image,
   title,
   children,
+  margin,
 }) => {
   console;
   return (
-    <section className="rounded-lg overflow-hidden border grid grid-flow-row lg:grid-flow-col shadow m-2 lg:h-96">
-      <div className="h-full">
+    <section
+      style={{ margin }}
+      className="rounded-lg overflow-hidden border grid grid-flow-row lg:grid-flow-col shadow m-2 lg:h-96"
+    >
+      <div className="h-full" hidden={!image}>
         <img className="w-full h-full object-cover" src={image} alt={title} />
       </div>
       <div className=" p-2 pr-6 overflow-hidden flex flex-col">
