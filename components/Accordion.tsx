a251e2efe9e5c3571e67180c1add4443a9a657c8
FF_Ian <ian@firstfactory.com>
2021-11-05 18:32:06 -0600

    Remove scroll context from Accordion as its no longer needed

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -1,13 +1,11 @@
 import React, { useContext } from "react";
 import { AccordionEntry, AccordionEntryProps } from "./AccordionEntry";
 import { darkCtx } from "./context/useDarkTheme";
-import { EnableScrollCtx } from "./layout/ScrollPages";
 
 export const Accordion: React.FC<{
   entries: AccordionEntryProps[];
 }> = ({ entries }) => {
   const [{ dark }] = useContext(darkCtx);
-  const [, setScroll] = useContext(EnableScrollCtx);
 
   dark;
   return (
@@ -15,8 +13,6 @@ export const Accordion: React.FC<{
       className={`w-full flex-1  overflow-y-auto overflow-x-hidden p-2 rounded-lg ${
         dark ? "bg-black" : "bg-gray-100"
       }`}
-      onMouseEnter={() => setScroll(false)}
-      onMouseLeave={() => setScroll(true)}
     >
       {entries.map((x, i) => (
         <AccordionEntry key={i} {...x} />
