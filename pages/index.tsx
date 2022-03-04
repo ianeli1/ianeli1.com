8a812314bc455d9117cb175512d422d74d1fd64b
Ian E <ian@teachable.com>
2022-03-04 15:07:23 -0600

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-import { darkCtx } from "components/context/useDarkTheme";
 import { Default } from "components/layout/default";
 import SeeThrough from "components/layout/SeeThrough";
-import React, { useContext } from "react";
+import React from "react";
 import { DopeBg } from "../components/DopeBg";
 import { MainIndex } from "../components/mainIndex";
 
